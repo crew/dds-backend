ce49c855752f9cae9d079ca1ddbf7b002bb1f29e
Will Nowak <compbrain@gmail.com>
2009-11-20 11:43:40 -0500

    New Django, New admin url format

--- dds/urls.py
+++ dds/urls.py
@@ -1,17 +1,13 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
+# Admin imports
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
+    # Include admin URLs
+    (r'^admin/', include(admin.site.urls)),
 
     # FIXME Development only, we will do apache later
     (r'^media/(?P<path>.*)$', 'django.views.static.serve',
