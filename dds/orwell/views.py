780183c21cdae14e11ccf69b9f424bc9d6644d12
Roderic Morris <roderyc@gmail.com>
2010-11-07 18:11:12 -0500

    Remove unused definitions of cli_* views.

--- dds/orwell/views.py
+++ dds/orwell/views.py
@@ -65,49 +65,6 @@ def client_activity_all_json(request):
         return HttpResponse(json.dumps(all, default=str))
     return HttpResponseNotAllowed(['GET'])
 
-@login_required
-def cli_manage_slide(request):
-    if request.method == 'POST':
-        f = CreateSlideForm(request.POST, request.FILES)
-        if f.is_valid():
-            try:
-                tf = tarfile.open(fileobj=request.FILES['bundle'])
-            except:
-                return HttpResponse('Not a tarfile! exitiing')
-            manifest = json.load(tf.extractfile('manifest.js'))
-            id = f.cleaned_data['id']
-            create = f.cleaned_data['mode'] == 'create'
-            if create and not id:
-                s = Slide(user=request.user,
-                          title='cli uploaded %s' % (tf.__hash__()),
-                          priority=-1,
-                          duration=-1)
-            elif not create and id:
-                s = Slide.objects.filter(id=id)[0]
-                if not s.allowed(request.user):
-                    return HttpResponse('Not allowed to modify slide')
-            else:
-                return HttpResponse('invalid: %s' % str(f.data))
-            s.populate_from_bundle(request.FILES['bundle'], tf)
-            return HttpResponse('Slide %s %sd'
-                                % (s.id, f.cleaned_data['mode']))
-    return HttpResponseNotAllowed(['POST'])
-
-@login_required
-def cli_list_slides(request):
-    if request.method == 'GET':
-        slidelist = []
-        for slide in Slide.objects.all():
-            s = {}
-            s['id'] = slide.id
-            s['title'] = slide.title
-            s['owner'] = str(slide.user)
-            slidelist.append(s)
-        return HttpResponse(json.dumps(slidelist))
-
-    return HttpResponseNotAllowed(['GET'])
-
-
 def web_form_slide_select(request) :
     return render_to_response('orwell/web-form-slide-select.html',
                               {"templates": Template.objects.all() },
