7f7b81a4458c58356f5391223e1c1ae3d2e1e725
Alex Lee <lee@ccs.neu.edu>
2011-02-22 21:06:09 -0500

    Fix CCIS ldap auth to use TLS.

--- dds/ccis/auth.py
+++ dds/ccis/auth.py
@@ -5,18 +5,22 @@ import logging
 
 LDAP_HOST = 'ldap://cluster.ldap.ccs.neu.edu'
 LDAP_DN ='uid=%s,ou=people,dc=ccs,dc=neu,dc=edu'
+LDAP_TLS = True
 
 
 def ldap_match(username, password):
     try:
         logging.info('Connecting to CCIS LDAP')
         conn = ldap.initialize(LDAP_HOST)
+        if LDAP_TLS:
+            # Start TLS (synchronously)
+            conn.start_tls_s()
         logging.info('Trying to bind %s' % username)
         result = conn.simple_bind_s(LDAP_DN % username, password)
         logging.info('Result = %s' % str(result))
         if result[0] == ldap.RES_BIND:
             return True
-    except ldap.LDAPError, e:
+    except ldap.LDAPError as e:
         logging.info(str(e))
         return False
 
@@ -24,11 +28,13 @@ def ldap_match(username, password):
 class CCISAuthBackend(object):
     def authenticate(self, username=None, password=None):
         if not username or not password:
-            return None
+            return
+        if not ldap_match(username, password):
+            return
         try:
-            user = User.objects.get(username=username)
-            if ldap_match(username, password):
-                return user
+            # XXX Access is limited by the existence of the User object
+            # in the database.
+            return User.objects.get(username=username)
         except User.DoesNotExist:
             return None
 
