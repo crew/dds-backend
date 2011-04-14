844f31a6eea812a4ac53ed64c062f399d6f6fb25
Roderic Morris <roderyc@gmail.com>
2011-04-13 22:55:49 -0400

    Add documentation to orwell/signalhandlers.py.

--- dds/orwell/signalhandlers.py
+++ dds/orwell/signalhandlers.py
@@ -1,10 +1,15 @@
-# vim: set shiftwidth=4 tabstop=4 softtabstop=4 expandtab :
+"""
+Signal handlers for various events on Orwell's models.
+"""
 import datetime
 import json
 
-
-
 def write_message(message):
+    """
+    Add a message to the queue of messages recieved by Harvest. message should
+    be an object translatable to json (so a dictionary, list, or some basic
+    value).
+    """
     # We import models here because of a circular dependency
     import models
     m = json.dumps(message)
@@ -17,25 +22,48 @@ def write_message(message):
         msg.save()
 
 def notify_playlist_change(playlist):
+    """
+    Add a message to the queue read by Harvest to notify it of a change to the
+    given playlist.
+    """
     write_message({'method': 'playlist', 'playlist':playlist.pk})
 
+# FIXME: slide_m_post_save and _pre_delete do the same thing. They could be the
+# same function and simply given to different signalers.
 def slide_m_post_save(sender, instance, created, **kwargs):
+    """
+    Notify Harvest that there was a change to each playlist that a given slide
+    belongs to after it is saved.
+    """
     for playlist in instance.playlists():
         notify_playlist_change(playlist)
 
 def slide_m_pre_delete(sender, instance, **kwargs):
+    """
+    Notify Harvest that there was a change to each playlist a given slide
+    belongs to before it is deleted.
+    """
     for playlist in instance.playlists():
         notify_playlist_change(playlist)
 
 def playlist_m_post_save(sender, instance, **kwargs):
+    """
+    Notify Harvest that there was a change to a playlist after it is saved.
+    """
     notify_playlist_change(instance)
 
 def client_m_post_save(sender, instance, **kwargs):
+    """
+    Notify Harvest that there was a change to a playlist after a client is
+    saved.
+    """
     notify_playlist_change(instance.playlist)
 
 def pis_m_post_save(sender, instance, **kwargs):
+    """
+    Notify Harvest that there was a change to a playlist  after one of its
+    playlist items is saved.
+    """
     notify_playlist_change(instance.playlist)
 
 pis_m_pre_delete = pis_m_post_save
-pig_m_post_save = pis_m_post_save
-pig_m_pre_delete = pis_m_pre_delete
