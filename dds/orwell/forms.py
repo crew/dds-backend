7e9686b811fcf274773b1c74b1f09c704f97db84
Roderic Morris <roderyc@gmail.com>
2011-04-13 23:27:22 -0400

    Add some docs to forms.py

--- dds/orwell/forms.py
+++ dds/orwell/forms.py
@@ -1,9 +1,15 @@
+"""
+Various forms and form fields used to submit data to Orwell.
+"""
 from django import forms
 from models import Slide, Client, Playlist, PlaylistItem
 import tarfile
 from StringIO import StringIO
 
 class BundleField(forms.FileField):
+    """
+    A form field that only accepts valid slide bundles.
+    """
     bundle_error_messages = {
             'nottar': u"The submitted file is not a tarfile.",
             'nomanifest': u"The submitted file does not contain a manifest.js file.",
@@ -25,15 +31,25 @@ class BundleField(forms.FileField):
         return f
 
 class CLICreateSlideForm(forms.Form):
+    """
+    A form used in the command line interface views to validate requests to
+    create slides from slidetool.py
+    """
     mode = forms.ChoiceField(choices=[('update', 'update'),
                                       ('create', 'create')])
     id = forms.IntegerField(required=False)
     bundle = BundleField()
 
 class CreateSlideForm(forms.Form):
+    """
+    A form used to allow users to create forms from bundles.
+    """
     bundle = BundleField()
 
 class CreatePDFSlideForm(forms.Form):
+    """
+    A form used to allow users to create forms from pdfs.
+    """
     title = forms.CharField()
     priority = forms.IntegerField(initial=1)
     duration = forms.IntegerField(initial=10, help_text="seconds")
@@ -41,21 +57,34 @@ class CreatePDFSlideForm(forms.Form):
     pdf = forms.FileField()
 
 class SlideEditForm(forms.Form):
+    """
+    A form used to allow users to edit a given slide.
+    """
     title = forms.CharField(required=False)
     expires_at = forms.DateTimeField(required=False)
     priority = forms.IntegerField()
     duration = forms.IntegerField()
 
 class ClientEditForm(forms.ModelForm):
+    """
+    A form used to allow users to edit a given client.
+    """
     class Meta:
         model = Client
         exclude = ('name','client_id')
 
 class PlaylistForm(forms.ModelForm):
+    """
+    A form used to allow users to create or edit a playlist.
+    """
     class Meta:
         model = Playlist
 
 class PlaylistItemForm(forms.ModelForm):
+    """
+    A form used to allow users to create or edit a playlist item within a
+    playlist.
+    """
     class Meta:
         model = PlaylistItem
         exclude = ('playlist',) # we will set this after form submission
